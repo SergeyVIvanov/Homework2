276ec4ff32505690d0fb3f83a7ade9d9617cd9cf
Sergey Ivanov <RelaxedMotion@gmail.com>
2022-11-25 22:28:40 +0300

    -

--- Insert.sql
+++ Insert.sql
@@ -1,37 +1,37 @@
-\c "SVI_Db1";
+п»ї\c "SVI_Db1";
 
 --
 -- clients
 --
 INSERT INTO public.clients(
     first_name, last_name, middle_name, birthday, id_doc_kind, id_doc_serial, id_doc_number, id_doc_issue_date, phone)
-    VALUES ('Бахтияров', 'Олег', 'Денисович', '1957-01-18', 21, '11 11', '222222', '2021-12-05', '+7(905)1112233');
+    VALUES ('Р‘Р°С…С‚РёСЏСЂРѕРІ', 'РћР»РµРі', 'Р”РµРЅРёСЃРѕРІРёС‡', '1957-01-18', 21, '11 11', '222222', '2021-12-05', '+7(905)1112233');
 INSERT INTO public.clients(
     first_name, last_name, middle_name, birthday, id_doc_kind, id_doc_serial, id_doc_number, id_doc_issue_date, phone)
-    VALUES ('Киселёв', 'Архип', 'Тарасович', '1974-08-06', 21, '33 33', '444444', '1994-06-14', '+7(903)0401014');
+    VALUES ('РљРёСЃРµР»С‘РІ', 'РђСЂС…РёРї', 'РўР°СЂР°СЃРѕРІРёС‡', '1974-08-06', 21, '33 33', '444444', '1994-06-14', '+7(903)0401014');
 INSERT INTO public.clients(
     first_name, last_name, middle_name, birthday, id_doc_kind, id_doc_serial, id_doc_number, id_doc_issue_date, phone)
-    VALUES ('Макарова', 'Эмилия', 'Куприяновна', '1998-01-08', 21, '55 55', '666666', '2011-09-19', '+7(905)1488991');
+    VALUES ('РњР°РєР°СЂРѕРІР°', 'Р­РјРёР»РёСЏ', 'РљСѓРїСЂРёСЏРЅРѕРІРЅР°', '1998-01-08', 21, '55 55', '666666', '2011-09-19', '+7(905)1488991');
 INSERT INTO public.clients(
     first_name, last_name, middle_name, birthday, id_doc_kind, id_doc_serial, id_doc_number, id_doc_issue_date, phone)
-    VALUES ('Мишин', 'Аполлон', 'Львович', '1984-03-27', 21, '77 77', '888888', '2020-02-04', '+7(920)9336831');
+    VALUES ('РњРёС€РёРЅ', 'РђРїРѕР»Р»РѕРЅ', 'Р›СЊРІРѕРІРёС‡', '1984-03-27', 21, '77 77', '888888', '2020-02-04', '+7(920)9336831');
 INSERT INTO public.clients(
     first_name, last_name, middle_name, birthday, id_doc_kind, id_doc_serial, id_doc_number, id_doc_issue_date, phone)
-    VALUES ('Воробьёва', 'Сара', 'Лаврентьевна', '1974-06-26', 21, '99 99', '123456', '2015-02-11', '+7(909)9167971');
+    VALUES ('Р’РѕСЂРѕР±СЊС‘РІР°', 'РЎР°СЂР°', 'Р›Р°РІСЂРµРЅС‚СЊРµРІРЅР°', '1974-06-26', 21, '99 99', '123456', '2015-02-11', '+7(909)9167971');
 
 --
 -- products
 --
 INSERT INTO public.products(name)
-    VALUES ('Текущий счёт');
+    VALUES ('РўРµРєСѓС‰РёР№ СЃС‡С‘С‚');
 INSERT INTO public.products(name)
-    VALUES ('Накопительный счёт');
+    VALUES ('РќР°РєРѕРїРёС‚РµР»СЊРЅС‹Р№ СЃС‡С‘С‚');
 INSERT INTO public.products(name)
-    VALUES ('Вклад Управляй +');
+    VALUES ('Р’РєР»Р°Рґ РЈРїСЂР°РІР»СЏР№ +');
 INSERT INTO public.products(name)
-    VALUES ('СберВклад');
+    VALUES ('РЎР±РµСЂР’РєР»Р°Рґ');
 INSERT INTO public.products(name)
-    VALUES ('Карта Visa Gold');
+    VALUES ('РљР°СЂС‚Р° Visa Gold');
 
 --
 -- client_product
